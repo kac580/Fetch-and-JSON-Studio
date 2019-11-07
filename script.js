c6a29f2bf2250be900cebf7c79835cd1d823ca94
kac580 <42049396+kac580@users.noreply.github.com>
2019-11-06 19:54:45 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        // console.log(response);
+        let json = [];
+        let index = 0;
+        response.json().then(function (json) {
+
+            // let name = json.firstName + ' ' + json.lastName;
+            const div = document.getElementById("container");
+            for (let index = 0; index < json.length; index++) {
+                div.innerHTML += `
+            <div class = "astronaut">
+                <div class = "bio">
+                    <h3> ${json[index].firstName + ' ' + json[index].lastName}</h3>
+                    <ul>
+                    <li> Hours in Space: ${json[index].hoursInSpace}</li>
+                    <li> Active: ${json[index].active}</li>
+                    <li> Skills: ${json[index].skills}</li>
+                    </ul>
+                </div>
+                    <img class = "avatar" src = ${json[index].picture}>
+
+            </div>
+            `;
+            };
+        });
+
+    });
+
+});
